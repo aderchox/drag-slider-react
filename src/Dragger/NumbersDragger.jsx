b45b781c6882014afc6ac4b17c70415b19400678
aderchox <aderchox@gmail.com>
2023-02-02 12:36:55 +0330

    dev: Adds snapping.

--- src/Dragger/NumbersDragger.jsx
+++ src/Dragger/NumbersDragger.jsx
@@ -6,11 +6,14 @@ const NumbersDragger = () => {
   const startXRef = useRef(null);
   const startScrollLeftRef = useRef(null);
   const sliderRef = useRef(null);
+  const activeSlideNumberRef = useRef(0);
+  const walkDirection = useRef(null);
 
   function dragStartHandler(e) {
     isDraggingRef.current = true;
     startXRef.current = e.pageX - sliderRef.current.offsetLeft;
     startScrollLeftRef.current = sliderRef.current.scrollLeft;
+    document.body.classList.add("slider-active");
   }
 
   function dragMoveHandler(e) {
@@ -19,19 +22,51 @@ const NumbersDragger = () => {
     }
     const walk = e.pageX - startXRef.current;
     sliderRef.current.scrollLeft = startScrollLeftRef.current - walk;
+    walkDirection.current = walk > 0 ? "left" : "right";
   }
 
   function endDrag(e) {
     isDraggingRef.current = false;
+    document.body.classList.remove("slider-active");
+    const fullWidth = parseInt(getComputedStyle(document.body).width, 10);
+    const widthIn = sliderRef.current.scrollLeft % fullWidth;
+    const widthInRatio = widthIn / fullWidth;
+    if (widthInRatio === 0) {
+      return;
+    }
+    if (walkDirection.current === "right") {
+      if (widthInRatio < 0.15) {
+        sliderRef.current.scrollLeft = sliderRef.current.scrollLeft - widthIn;
+      } else {
+        sliderRef.current.scrollLeft =
+          sliderRef.current.scrollLeft - widthIn + fullWidth;
+        activeSlideNumberRef.current++;
+      }
+    } else {
+      if (widthInRatio < 0.85) {
+        sliderRef.current.scrollLeft = sliderRef.current.scrollLeft - widthIn;
+        activeSlideNumberRef.current--;
+      } else {
+        sliderRef.current.scrollLeft =
+          sliderRef.current.scrollLeft - widthIn + fullWidth;
+      }
+    }
+  }
+
+  function windowResizeHandler() {
+    const bodyWidth = parseInt(getComputedStyle(document.body).width, 10);
+    sliderRef.current.scrollLeft = activeSlideNumberRef.current * bodyWidth;
   }
 
   useEffect(() => {
     window.addEventListener("mousemove", dragMoveHandler);
     window.addEventListener("mouseup", endDrag);
+    window.addEventListener("resize", windowResizeHandler);
 
     return () => {
       window.removeEventListener("mousemove", dragMoveHandler);
       window.removeEventListener("mouseup", endDrag);
+      window.removeEventListener("resize", windowResizeHandler);
     };
   }, []);
 
@@ -40,6 +75,8 @@ const NumbersDragger = () => {
       <DragItem>1</DragItem>
       <DragItem>2</DragItem>
       <DragItem>3</DragItem>
+      <DragItem>4</DragItem>
+      <DragItem>5</DragItem>
     </div>
   );
 };
